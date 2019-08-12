40890e38a6f8144d550e9cbf8582aa7cf1850175
Connor Svrcek <csvrcek@umich.edu>
2019-08-12 13:49:21 -0400

    Updates

---
+++ precache-manifest.3c479354391fb39a3b30b45c25c3672d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac12967b9114aca81e942f62deb2aee",
+    "url": "/0118a21553a1eb38f613.worker.js"
+  },
+  {
+    "revision": "a6ae23a655d48735d9db1f79977820e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a24bfd5f9f3b27765713",
+    "url": "/static/css/main.c75ae443.chunk.css"
+  },
+  {
+    "revision": "6aa0a6412ca8a0192648",
+    "url": "/static/js/2.d487855e.chunk.js"
+  },
+  {
+    "revision": "a24bfd5f9f3b27765713",
+    "url": "/static/js/main.13bfe7a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5fbafcd66af7d4d383b735958656a2ad",
+    "url": "/static/media/Connor Svrcek Resume.5fbafcd6.pdf"
+  },
+  {
+    "revision": "2947335e8621388ba95ac3c06911b092",
+    "url": "/static/media/cedar_point.2947335e.jpg"
+  },
+  {
+    "revision": "51f9fe6369f5bbbb8b143561175ce123",
+    "url": "/static/media/hackers_exec.51f9fe63.jpg"
+  },
+  {
+    "revision": "72422f4c58de0fd0ad5838ee897532fa",
+    "url": "/static/media/hacking.72422f4c.jpg"
+  },
+  {
+    "revision": "b30093e39ef49874a72c964e4be40735",
+    "url": "/static/media/hoola.b30093e3.jpg"
+  },
+  {
+    "revision": "cf28b0ff3902bb7a364edf4f7a6b5963",
+    "url": "/static/media/kayak.cf28b0ff.jpg"
+  },
+  {
+    "revision": "ab3041702e7931f10dc9842960d6503c",
+    "url": "/static/media/log.ab304170.jpg"
+  },
+  {
+    "revision": "917e7e1cf3c2a7e48841648c5b823156",
+    "url": "/static/media/profile_image.917e7e1c.png"
+  },
+  {
+    "revision": "9963842d97e132c94e7b718f57746050",
+    "url": "/static/media/shades.9963842d.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
-"use strict";var precacheConfig=[["/b898199c77b935089e6c.worker.js","650d519719195bfde26bc7cc3e0a3536"],["/index.html","9dc5ba639f59c085fb5ff915d579dda8"],["/static/css/main.b012ba01.css","3f45f541949b410fe9911142f5480bf0"],["/static/js/0.d2f0cb63.chunk.js","4a440604af57c1b2dbaa05e2e2ab7f2e"],["/static/js/1.8f6e74d6.chunk.js","76541608be1451c10cea0036010edf8b"],["/static/js/main.0b066c55.js","68a0296a55b4cd36e0baf473b4c5ba97"],["/static/media/Connor Svrcek Resume.75bf1d8c.pdf","75bf1d8c5cbf9486f0c55ca42bdd7931"],["/static/media/cedar_point.88fb58e3.jpg","88fb58e3806e56e2a84607de4d011c9b"],["/static/media/hackers_exec.51f9fe63.jpg","51f9fe6369f5bbbb8b143561175ce123"],["/static/media/hacking.72422f4c.jpg","72422f4c58de0fd0ad5838ee897532fa"],["/static/media/hoola.b8b0b655.jpg","b8b0b655349440ce8199aff377c6b0c1"],["/static/media/profile_image.97fe104a.png","97fe104a68ffd23ce1603e5d3e13cde8"],["/static/media/shades.9963842d.JPG","9963842d97e132c94e7b718f57746050"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(t){return t.redirected?("body"in t?Promise.resolve(t.body):t.blob()).then(function(e){return new Response(e,{headers:t.headers,status:t.status,statusText:t.statusText})}):Promise.resolve(t)},createCacheKey=function(e,t,n,r){var a=new URL(e);return r&&a.pathname.match(r)||(a.search+=(a.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),a.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,n){var t=new URL(e);return t.hash="",t.search=t.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(t){return n.every(function(e){return!e.test(t[0])})}).map(function(e){return e.join("=")}).join("&"),t.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],r=new URL(t,self.location),a=createCacheKey(r,hashParamName,n,/\.\w{8}\./);return[r.toString(),a]}));function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(r){return setOfCachedUrls(r).then(function(n){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(t){if(!n.has(t)){var e=new Request(t,{credentials:"same-origin"});return fetch(e).then(function(e){if(!e.ok)throw new Error("Request for "+t+" returned a response with status "+e.status);return cleanResponse(e).then(function(e){return r.put(t,e)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var n=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(t){return t.keys().then(function(e){return Promise.all(e.map(function(e){if(!n.has(e.url))return t.delete(e)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(t){if("GET"===t.request.method){var e,n=stripIgnoredUrlParameters(t.request.url,ignoreUrlParametersMatching),r="index.html";(e=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,r),e=urlsToCacheKeys.has(n));var a="/index.html";!e&&"navigate"===t.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],t.request.url)&&(n=new URL(a,self.location).toString(),e=urlsToCacheKeys.has(n)),e&&t.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(e){return console.warn('Couldn\'t serve response for "%s" from cache: %O',t.request.url,e),fetch(t.request)}))}});
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.3c479354391fb39a3b30b45c25c3672d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
