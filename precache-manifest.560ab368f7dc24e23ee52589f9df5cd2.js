1d24160ae7fee48748217c66f037502720503c44
Connor Svrcek <csvrcek@umich.edu>
2020-03-11 18:25:40 +0000

    Updates

---
+++ precache-manifest.560ab368f7dc24e23ee52589f9df5cd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac12967b9114aca81e942f62deb2aee",
+    "url": "/0118a21553a1eb38f613.worker.js"
+  },
+  {
+    "revision": "3548f823550807694b6a7723b4d34af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4572421704a2adfdec3",
+    "url": "/static/css/main.e8104f57.chunk.css"
+  },
+  {
+    "revision": "5ef27be6ee22d1e5a8e8",
+    "url": "/static/js/2.79be48d7.chunk.js"
+  },
+  {
+    "revision": "d4572421704a2adfdec3",
+    "url": "/static/js/main.cfefd63f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74a01b52e8907c1c9935602b41761d21",
+    "url": "/static/media/profile_image.74a01b52.JPG"
+  },
+  {
+    "revision": "480f46faa86b76f7e581a1492edc2b9f",
+    "url": "/static/media/resume.480f46fa.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4e9f4e119d2681c93a92c65da6f3486.js"
+  "/precache-manifest.560ab368f7dc24e23ee52589f9df5cd2.js"
 );
 
 self.addEventListener('message', (event) => {
